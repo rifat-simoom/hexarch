f6f15d171ef54a324e73e51d21fd87cb8c08bcc7
rifat <rifat.simoom@softrobotics.com.bd>
2025-02-22 18:27:28 +0600

    Refact : Updated Name

--- src/BuildingBlocks/Logging/SeriLogger.cs
+++ src/BuildingBlocks/Logging/SeriLogger.cs
@@ -20,7 +20,7 @@ public static class SeriLogger
                 .WriteTo.Elasticsearch(
                     new ElasticsearchSinkOptions(new Uri(elasticUri))
                     {
-                        IndexFormat = $"suucat-applogs-{context.HostingEnvironment.ApplicationName?.ToLowerInvariant().Replace(".", "-")}" +
+                        IndexFormat = $"hexarch-applogs-{context.HostingEnvironment.ApplicationName?.ToLowerInvariant().Replace(".", "-")}" +
                                       $"-{context.HostingEnvironment.EnvironmentName?.ToLowerInvariant().Replace(".", "-")}" +
                                       $"-{DateTime.UtcNow:yyyy-MM}",
                         AutoRegisterTemplate = true,
